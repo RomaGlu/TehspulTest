d7a5a5aab128ddbdb218fc0e17ca4e043da3dad0
Roman Hlukharou <roma.glu.69@gmail.com>
2024-01-02 00:13:17 +0300

    end setup MVC architecture

---
+++ TehspulTest/Sources/App/Model/OnboardingModel.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingModel.swift
+//  TehspulTest
+//
+//  Created by Роман Глухарев on 2.01.24.
+//
+
+import Foundation
---
+++ TehspulTest/Sources/App/View/OnboardingView.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingView.swift
+//  TehspulTest
+//
+//  Created by Роман Глухарев on 2.01.24.
+//
+
+import Foundation
