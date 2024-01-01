5522c811a5f60c100b9f1eafe421701607d4f5a2
Roman Hlukharou <roma.glu.69@gmail.com>
2024-01-01 23:58:40 +0300

    Initial Commit

---
+++ TehspulTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TehspulTest
+//
+//  Created by Роман Глухарев on 1.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
